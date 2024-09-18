e86e43fb905bc505999af8ece2c9279aac69a8ac
julioquintanazaez <jupyterdevs2022@gmail.com>
2024-09-18 06:15:56 -0430

    Functional version v0.1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import mechanicalsoup
+import os
+import wget
 
 browser = mechanicalsoup.StatefulBrowser()
 
@@ -22,3 +24,43 @@ browser["q"] = sesrch_term
 #submit/click search
 browser.launch_browser()
 response = browser.submit_selected()
+
+print("New url", browser.get_url())
+print("My responese:", response.text[:500])
+
+#Open a new url
+new_url = browser.get_url()
+browser.open(new_url)
+
+#Get the HTML
+page = browser.get_current_page()
+all_images = page.find_all("img")
+
+print(all_images)
+
+#Target the source attribute
+image_source = []
+for image in all_images:
+    _image = image.get("src")
+    image_source.append(_image)
+
+print(image_source)
+
+#Remove rong format url from the list
+image_source = [image for image in image_source if image.startwith("https")]
+print(image_source)
+
+path = os.getcwd()
+path = os.path.join(path, search_terms + "s")
+
+#Creat th dicectory
+os.mkdir(path)
+
+print(path)
+
+#Download images
+counter = 0
+for image in image_source:
+    save_as = os.path.join(path, search_terms + str(counter) + "jpg")
+    wget.download(image, save_as)
+    counter += 1
